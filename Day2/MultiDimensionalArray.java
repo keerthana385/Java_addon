7523a02643db6ba524e2e296fa5bcf9dc1705203
keerthana385 <2326kb22@kgcas.com>
2025-06-06 14:48:33 +0530

    Add files via upload

---
+++ Day2/MultiDimensionalArray.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class MultiDimensionalArray {
+	public static void main(String[] args) {
+		int [][] num= {{1, 2},{10, 20}};
+		
+		for(int i=0;i<=num.length-1;i++) {
+			for(int j=0;j<=num.length-1;j++) {
+				System.out.print(num[i][j]+" ");
+			}
+			System.out.println(" ");
+		}
+	}
+}
