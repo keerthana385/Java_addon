5a95e476c234209e24c9ab80ef7c6d44fe282b8e
keerthana385 <2326kb22@kgcas.com>
2025-06-09 18:50:04 +0530

    Update Task2.java

--- Day3/Task2.java
+++ Day3/Task2.java
@@ -7,7 +7,7 @@ public class Task2 {
 	}
 	void salary(String empolyee, double fixed,int hrs) {
 		System.out.println("State of empolyee:"+empolyee);
-		System.out.println("Salary:"+fixed/2);
+		System.out.println("Salary:"+fixed);
 		System.out.println("No.of hrs wroked:"+hrs);
 	}
 	void salary(String empolyee,int hrs,int rate) {
@@ -21,8 +21,9 @@ public class Task2 {
 	public static void main(String[] args) {
 		Task2 t=new Task2();
 		t.salary("Full-time", 50000);
-		t.salary("Part-time", 50000, 6);
+		t.salary("Part-time", 750, 6);
 		t.salary("Freelancer", 7, 1000);
 	}
 
 }
+
